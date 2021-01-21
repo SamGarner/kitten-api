5d40b448e284c26d4698e7f722b0fa23e4dc9708
SamGarner <samuel.w.garner@gmail.com>
2021-01-20 20:46:48 -0600

    Feat: Define all seven RESTful actions in KittensController.

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,3 +1,55 @@
 class KittensController < ApplicationController
+  def index
+    @kittens = Kitten.all
+  end
 
+  def new
+    @kitten = Kitten.new
+  end
+
+  def create
+    @kitten = Kitten.new(allowed_kitten_params)
+    if @kitten.save?
+      flash[:success] = 'Saved to database!'
+      redirect_to @kitten
+    else
+      flash[:error] = 'Error. Please double-check your entry.'
+      render :new
+    end
+  end
+
+  def show
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def edit
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def update
+    @kitten = Kitten.find(params[:id])
+    if @kitten.update(allowed_kitten_params)
+      redirect_to @kitten
+    else
+      flash[:error] = 'Error. Please double-check your entry.'
+      render :edit
+    end
+  end
+
+  def destroy
+    @kitten = Kitten.find(params[:id])
+    if @kitten.destroy
+      flash[:success] = 'Delete successful.'
+      redirect_to :root
+    else
+      flash[:error] = 'Delete failed. Please try again.'
+      redirect_to @kitten
+    end
+  end
+
+  private
+
+  def allowed_kitten_params
+    params.require(:kitten).permit(:name, :age, :softness, :cuteness)
+  end
 end
